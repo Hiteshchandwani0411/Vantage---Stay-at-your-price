822486d47a7659a9acd1db094480aeecc79d1f1e
Hitesh <hitesh.chandwani251133@gmail.com>
2026-05-27 00:10:02 +0530

    fixes: Controller of wishlist

--- controllers/wishlist.controller.js
+++ controllers/wishlist.controller.js
@@ -1,31 +1,40 @@
 const Wishlist = require("../models/wishlist");
 
 module.exports.addToWishlist = async (req, res) => {
-  let listingId = req.params.listingId;
-  let userId = req.user ? req.user._id : "65f1a2b3c4d5e6f7a8b9c0d1";
+    try {
 
-  await Wishlist.create({
-    userId,
-    listingId,
-  });
-
-  res
-    .status(201)
-    .json({
-      success: true,
-      message: "Added to wishlist",
-      data: newWishlistItem,
-    });
+        let listingId = req.params.listingId;
+        let userId = req.user ? req.user._id : "65f1a2b3c4d5e6f7a8b9c0d1";
+      
+        await Wishlist.create({
+          userId,
+          listingId,
+        });
+      
+        res
+          .status(201)
+          .json({
+            success: true,
+            message: "Added to wishlist",
+          });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
 };
 
 module.exports.removeListing = async (req, res) => {
-  let { listingId } = req.params;
-  let userId = req.user ? req.user._id : "65f1a2b3c4d5e6f7a8b9c0d1";
+  try {
+    let { listingId } = req.params;
+    let userId = req.user ? req.user._id : "65f1a2b3c4d5e6f7a8b9c0d1";
 
-  // findByIdAndDelete ki jagah deleteOne use karenge kyunki hum listingId se delete kar rahe hain
-  let removedListing = await Wishlist.deleteOne({
-    userId: userId,
-    listingId: listingId,
-  });
-  console.log(removedListing);
+    // findByIdAndDelete ki jagah deleteOne use karenge kyunki hum listingId se delete kar rahe hain
+    let removedListing = await Wishlist.deleteOne({
+      userId: userId,
+      listingId: listingId,
+    });
+    console.log(removedListing);
+    res.status(200).json({ success: true, message: "Removed from wishlist" });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
--- router/wishlist.js
+++ router/wishlist.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { addToWishlist, removeListing } = require("../controllers/wishlist.controller");
 const router = express.Router();
 
-
 router.post("/wishlist/add/:listingId", addToWishlist);
 router.delete("/wishlist/remove/:listingId", removeListing);
 
